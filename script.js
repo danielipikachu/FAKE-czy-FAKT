3556ecf38457b627ea8aeba058a58187bdf03f0f
danielipikachu <danielkasprowicz51@gmail.com>
2026-06-04 18:40:28 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,48 @@
+p0.addEventListener("click",function(){
+    var widoczny = document.getElementsByClassName("prostokat-widoczny");
+    while(widoczny.length){
+        widoczny[0].classList.remove("prostokat-widoczny")
+    }
+    prostokat0.classList.add("prostokat-widoczny")
+})
+
+p1.addEventListener("click",function(){
+    var widoczny = document.getElementsByClassName("prostokat-widoczny");
+    while(widoczny.length){
+        widoczny[0].classList.remove("prostokat-widoczny")
+    }
+    prostokat1.classList.add("prostokat-widoczny")
+})
+
+p2.addEventListener("click",function(){
+    var widoczny = document.getElementsByClassName("prostokat-widoczny");
+    while(widoczny.length){
+        widoczny[0].classList.remove("prostokat-widoczny")
+    }
+    prostokat2.classList.add("prostokat-widoczny")
+})
+
+p3.addEventListener("click",function(){
+    var widoczny = document.getElementsByClassName("prostokat-widoczny");
+    while(widoczny.length){
+        widoczny[0].classList.remove("prostokat-widoczny")
+    }
+    prostokat3.classList.add("prostokat-widoczny")
+})
+
+p4.addEventListener("click",function(){
+    var widoczny = document.getElementsByClassName("prostokat-widoczny");
+    while(widoczny.length){
+        widoczny[0].classList.remove("prostokat-widoczny")
+    }
+    prostokat4.classList.add("prostokat-widoczny")
+})
+
+p5.addEventListener("click",function(){
+    var widoczny = document.getElementsByClassName("prostokat-widoczny");
+    while(widoczny.length){
+        widoczny[0].classList.remove("prostokat-widoczny")
+    }
+    prostokat5.classList.add("prostokat-widoczny")
+})
+
